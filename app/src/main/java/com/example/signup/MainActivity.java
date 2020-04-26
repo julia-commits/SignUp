6218eef6eb16147e1fba7eff3377b8c793af46c1
AmaBirago <julia_asante@outlook.com>
2020-04-26 21:51:27 +0100

    fixed

--- app/src/main/java/com/example/signup/MainActivity.java
+++ app/src/main/java/com/example/signup/MainActivity.java
@@ -1,27 +1,29 @@
 package com.example.signup;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-        textView = findViewById(R.id.textView3);
+        textView = findViewById(R.id.textViewIn);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, signupActivity.class);
+                startActivity(intent);
+                
+            }
+        });
 
-    }
-
-    public void createAccount (View view){
-        Intent intent = new Intent (this, signupActivity.class);
-        startActivity(intent);
     }
 }
