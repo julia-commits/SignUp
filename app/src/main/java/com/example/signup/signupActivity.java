dfcb5519884f667c03dd264cf496f7346515c54b
AmaBirago <julia_asante@outlook.com>
2020-04-26 22:00:33 +0100

    updated

--- app/src/main/java/com/example/signup/signupActivity.java
+++ app/src/main/java/com/example/signup/signupActivity.java
@@ -4,19 +4,27 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class signupActivity extends Activity {
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_activity);
 
+        textView = findViewById(R.id.textView3);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(signupActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 
-
-    public void login (View view){
-        Intent intent = new Intent (this, MainActivity.class);
-        startActivity(intent);
-    }
 }
