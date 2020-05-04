b7527aa57cc26dd32fa880806d46e8655114d857
AmaBirago <julia_asante@outlook.com>
2020-05-04 18:52:54 +0100

    fixed

--- app/src/main/java/com/example/signup/MainActivity.java
+++ app/src/main/java/com/example/signup/MainActivity.java
@@ -7,13 +7,19 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+         TextInputLayout hintBox = findViewById(R.id.search);
+         hintBox.setHint("Email");
 
         textView = findViewById(R.id.textViewIn);
         textView.setOnClickListener(new View.OnClickListener() {
